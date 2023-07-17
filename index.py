03e6e5f87f03e7a06d065d70f15fc876dfa1e11b
naarkz <naarkz@michie.dev>
2023-07-17 22:38:15 +0800

    refactor: Add db credentials to env

--- index.py
+++ index.py
@@ -2,13 +2,16 @@ import psycopg2
 import json
 import re
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 con = psycopg2.connect(
-    database="psgc",
-    user="postgres",
-    password="3423",
-    host="localhost",
-    port="5432"
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
 )
 
 # Define a helper function to extract the date from the filename
