75a9667460e4b6b788d8260e2a8ceb6a88d1e021
naarkz <naarkz@michie.dev>
2023-07-17 20:57:41 +0800

    feat: Add script to populate psgc db

---
+++ index.py
@@ -0,0 +1,78 @@
+import psycopg2
+import json
+import re
+import os
+
+con = psycopg2.connect(
+    database="psgc",
+    user="postgres",
+    password="3423",
+    host="localhost",
+    port="5432"
+)
+
+# Define a helper function to extract the date from the filename
+
+
+def extract_date_from_filename(filename):
+    match = re.search(r"\d{4}-\d{2}-\d{2}", filename)
+    if match:
+        return match.group()
+    else:
+        return None
+
+# Define a helper function to construct the JSON file path
+
+
+def get_json_file_path(level_name):
+    directory = "psgc_json"
+    files = os.listdir(directory)
+    for file in files:
+        if file.startswith(level_name):
+            file_date = extract_date_from_filename(file)
+            if file_date:
+                return os.path.join(directory, file)
+    return None
+
+
+# Load the JSON data for each geographic level
+with open(get_json_file_path("regions")) as json_file:
+    regions_data = json.load(json_file)
+
+with open(get_json_file_path("provinces")) as json_file:
+    provinces_data = json.load(json_file)
+
+with open(get_json_file_path("cities")) as json_file:
+    cities_data = json.load(json_file)
+
+with open(get_json_file_path("municipalities")) as json_file:
+    municipalities_data = json.load(json_file)
+
+with open(get_json_file_path("barangays")) as json_file:
+    barangays_data = json.load(json_file)
+
+with open(get_json_file_path("submunicipalities")) as json_file:
+    submunicipalities_data = json.load(json_file)
+
+
+def insert_data(table_name, data):
+    if table_name == "submunicipalities":
+        return  # Skip insertion for the "submunicipalities" table
+    cursor = con.cursor()
+    for row in data:
+        placeholders = ', '.join(['%s'] * len(row))
+        columns = ', '.join(row.keys())
+        query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders}) ON CONFLICT DO NOTHING"
+        values = tuple(row.values())
+        cursor.execute(query, values)
+    con.commit()
+
+
+insert_data("regions", regions_data)
+insert_data("provinces", provinces_data)
+insert_data("cities", cities_data)
+insert_data("municipalities", municipalities_data)
+insert_data("barangays", barangays_data)
+insert_data("submunicipalities", submunicipalities_data)
+
+con.close()
